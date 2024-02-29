46309fbb7969231bedee031a2014bb44ed8bf1d6
codewithadityaa <114819528+codewithadityaa@users.noreply.github.com>
2024-03-01 01:07:42 +0530

    Add files via upload

---
+++ 05-menu/indian-menu-updated/src/data.jsx
@@ -0,0 +1,139 @@
+const menu = [
+    {
+      id: 1,
+      title: 'idli',
+      category: 'breakfast',
+      price: "₹60",
+      img: 'idlii.avif',
+      desc: ` Steamed rice cakes made from fermented rice and urad dal batter, often served with coconut chutney and sambar.`,
+    },
+    {
+      id: 11,
+      title: 'Indian-thali',
+      category: 'lunch',
+      price: "₹300",
+      img: 'thali.jpg',
+      desc: ` Variety of dishes, including roti, rice, dal, assorted curries, raita, pickle, papad, chutney, paneer, optional meat, sweets, buttermilk, and salad.`,
+    },
+    {
+      id: 13,
+      title: 'Paneer-Butter-masala',
+      category: 'lunch',
+      price: "₹260",
+      img: 'bnaan.jpg',
+      desc: `A soft leavened bread brushed with butter, with the exquisite richness of Butter Paneer Masala.`,
+    },
+    {
+      id: 2,
+      title: 'biryani',
+      category: 'lunch',
+      price: "₹240",
+      img: 'biryani.avif',
+      desc: `Fragrant rice dish cooked with aromatic spices, basmati rice, and meat or vegetables. It's often accompanied by raita (yogurt with spices).`,
+    },
+    {
+      id: 4,
+      title: 'masala dosa',
+      category: 'breakfast',
+      price: "₹80",
+      img: 'dosa.avif',
+      desc: `Thin, crispy rice crepes made from a fermented batter of rice and urad dal, usually served with coconut chutney and sambar. `,
+    },
+    {
+      id: 6,
+      title: 'strawberry shake',
+      category: 'shakes',
+      price: "₹120",
+      img: 'pinkshake.jpg',
+      desc: `blend of fresh strawberries, milk, and a touch of sweetness, whipped into a creamy delight.`,
+    },
+   
+    {
+      id: 3,
+      title: 'paratha',
+      category: 'breakfast',
+      price: "₹80",
+      img: 'parotha.jpg',
+      desc: `Flatbreads stuffed with various fillings such as spiced potatoes, paneer (Indian cottage cheese), or minced meat.`,
+    },
+    
+    {
+      id: 5,
+      title: 'daal-khichdi',
+      category: 'lunch',
+      price: "₹180",
+      img: 'daalbat.jpg',
+      desc: `A comforting one-pot blend of rice, moong dal, and veggies, seasoned with cumin and mustard seeds. `,
+    },
+    {
+      id: 16,
+      title: 'oreo-shake',
+      category: 'shakes',
+      price: "₹140",
+      img: 'o.jpg',
+      desc: `Delight in the velvety goodness of an Oreo Shake, blending the iconic chocolate sandwich cookies with milk for a decadent and satisfying treat.`,
+    },
+
+    {
+      id: 7,
+      title: 'dhokla',
+      category: 'breakfast',
+      price: "₹70",
+      img: 'dhokla.avif',
+      desc: `Steamed fermented cake made from rice and chickpea flour, typically served with mint chutney.`,
+    },
+    
+    {
+      id: 8,
+      title: 'puri-bhaji',
+      category: 'lunch',
+      price: "₹110",
+      img: 'puribhaji.avif',
+      desc: `Savor the delightful union of fluffy Puri and spiced Potato Bhaji for a satisfying and flavorful dining experience. `,
+    },
+    
+    {
+      id: 9,
+      title: 'chole-bhature',
+      category: 'lunch',
+      price: "₹130",
+      img: 'chole.avif',
+      desc: `A North Indian dish consisting of spiced chickpeas (chole) served with deep-fried bread (bhatura).`,
+    },
+    {
+      id: 14,
+      title: 'Upmav',
+      category: 'breakfast',
+      price: "₹70",
+      img: 'u.jpg',
+      desc: `A savory porridge made from semolina (sooji) or coarse rice flour, cooked with vegetables and seasoned with mustard seeds, curry leaves, and spices.`,
+    },
+    {
+      id: 10,
+      title: 'Choco-shake',
+      category: 'shakes',
+      price: "₹110",
+      img: 'chocoshake.jpg',
+      desc: `Indulge in a rich and velvety concoction of chocolate goodness, crafted with milk and a hint of sweetness.`,
+    },
+    
+    {
+      id: 12,
+      title: 'Banana-shake',
+      category: 'shakes',
+      price: "₹110",
+      img: 'bananashake.jpg',
+      desc: `A Banana Shake is a creamy delight made with ripe bananas, milk, and a touch of sweetness, blended to perfection.`,
+    },
+    {
+      id: 15,
+      title: 'Poha',
+      category: 'breakfast',
+      price: "₹70",
+      img: 'p.jpg',
+      desc: `Flattened rice cooked with mustard seeds, curry leaves, turmeric, and garnished with peanuts, chopped onions, and coriander leaves.`,
+    },
+    
+  ];
+  export default menu;
+ 
