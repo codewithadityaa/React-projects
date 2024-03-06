1c31fca174a1e34899837fa53364e44586d26569
codewithadityaa <114819528+codewithadityaa@users.noreply.github.com>
2024-03-07 01:28:04 +0530

    Add files via upload

---
+++ 15-cocktails/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolore, laborum amet neque perferendis distinctio nemo assumenda culpa aspernatur nobis sunt suscipit eveniet. Adipisci libero fugit earum aliquid a excepturi quam similique odio odit alias aspernatur repudiandae error asperiores dicta explicabo reprehenderit, sequi esse? Possimus perferendis omnis laudantium similique asperiores.</p>
+    </section>
+  )
+}
+
+export default About
